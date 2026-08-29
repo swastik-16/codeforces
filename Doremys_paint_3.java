84f2936a0ec6cf4c75364458144c6d62c0e02dd2
swastik-16 <swastik16mukherjee@gmail.com>
2026-08-29 14:53:42 +0530

    Updates

--- Doremys_paint_3.java
+++ Doremys_paint_3.java
@@ -9,25 +9,20 @@ public class Doremys_paint_3 {
 
         while (t-- > 0) {
             int n = sc.nextInt();
-
             HashMap<Integer, Integer> freq = new HashMap<>();
-
             for (int i = 0; i < n; i++) {
                 int x = sc.nextInt();
                 freq.put(x, freq.getOrDefault(x, 0) + 1);
             }
-
             if (freq.size() == 1) {
                 System.out.println("Yes");
             } 
             else if (freq.size() == 2) {
                 int[] counts = new int[2];
                 int idx = 0;
-
                 for (int count : freq.values()) {
                     counts[idx++] = count;
                 }
-
                 if (Math.abs(counts[0] - counts[1]) <= 1) {
                     System.out.println("Yes");
                 } else {
