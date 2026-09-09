d63c7c44167815a93e7a7a5763d6f1703ff336ca
swastik-16 <swastik16mukherjee@gmail.com>
2026-09-09 13:33:43 +0530

    Updates

--- Atcoder/Not_divisible_170_D.java
+++ Atcoder/Not_divisible_170_D.java
@@ -1,24 +1,18 @@
-package Atcoder;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
+import java.io.*;
 
 public class Not_divisible_170_D {
+
     static class FastScanner {
         private final InputStream in = System.in;
         private final byte[] buffer = new byte[1 << 16];
-        private int ptr = 0;
-        private int len = 0;
+        private int ptr = 0, len = 0;
 
         private int read() throws IOException {
             if (ptr >= len) {
                 len = in.read(buffer);
                 ptr = 0;
-
                 if (len <= 0) return -1;
             }
-
             return buffer[ptr++];
         }
 
@@ -28,38 +22,60 @@ public class Not_divisible_170_D {
                 c = read();
             } while (c <= ' ');
 
-            int sign = 1;
-            if (c == '-') {
-                sign = -1;
-                c = read();
-            }
-
-            int result = 0;
-
+            int num = 0;
             while (c > ' ') {
-                result = result * 10 + (c - '0');
+                num = num * 10 + (c - '0');
                 c = read();
             }
-
-            return result * sign;
+            return num;
         }
     }
+
     public static void main(String[] args) throws Exception {
         FastScanner fs = new FastScanner();
+
         int n = fs.nextInt();
-        int[] arr = new int[n];
-        for(int i=0;i<n;i++){
-            arr[i] = fs.nextInt();
+        int MAX = 1_000_000;
+
+        int[] freq = new int[MAX + 1];
+        int maxValue = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x = fs.nextInt();
+            freq[x]++;
+            maxValue = Math.max(maxValue, x);
         }
-        Arrays.sort(arr);
-        int[] visited = new int[arr[n-1]+1];
-        for(int i=0;i<n;i++){
-            for(int j=1;j<arr[j]/j;j++){
-                if(arr[i]%j==0){
-                    visited[j] = 1;
-                    visited[arr[i]/j]=1;
+
+        boolean[] invalid = new boolean[MAX + 1];
+
+        for (int x = 1; x <= maxValue; x++) {
+
+            if (freq[x] == 0) continue;
+
+            // If x occurs multiple times, another x divides it
+            if (freq[x] > 1) {
+                invalid[x] = true;
+            }
+
+            // Mark all larger multiples of x as invalid
+            for (int multiple = 2 * x;
+                 multiple <= maxValue;
+                 multiple += x) {
+
+                if (freq[multiple] > 0) {
+                    invalid[multiple] = true;
                 }
             }
         }
+
+        int answer = 0;
+
+        for (int x = 1; x <= maxValue; x++) {
+            if (freq[x] == 1 && !invalid[x]) {
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
     }
 }
